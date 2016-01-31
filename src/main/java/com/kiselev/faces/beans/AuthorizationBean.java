7a1ad1b8400c3a0b5f64ef8b117416750b8aa02c
vadim8kiselev <vadim8kiselev@gmail.com>
2016-01-31 18:37:51 +0300

    Add validation of data

--- src/main/java/com/kiselev/faces/beans/AuthorizationBean.java
+++ src/main/java/com/kiselev/faces/beans/AuthorizationBean.java
@@ -2,18 +2,13 @@ package com.kiselev.faces.beans;
 
 import com.kiselev.faces.dao.DAO;
 import com.kiselev.faces.dao.entities.ProfileEntity;
+import com.kiselev.faces.validators.Validator;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.imageio.ImageIO;
-import javax.persistence.PersistenceException;
 import javax.servlet.http.HttpSession;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.io.Serializable;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 @ManagedBean(name = "authorizationBean")
 @SessionScoped
@@ -36,13 +31,6 @@ public class AuthorizationBean implements Serializable {
 
     }
 
-    private void resetData() {
-        username = null;
-        password = null;
-        secondPassword = null;
-        inMessage = null;
-        upMessage = null;
-    }
 
     public boolean isRegistered() {
         return registered;
@@ -133,89 +121,108 @@ public class AuthorizationBean implements Serializable {
     }
 
     public String signin() {
-        if (!"".equals(username.trim()) && !"".equals(password.trim())) {
-            username = username.trim();
-            password = password.trim();
+        username = username.trim();
+        password = password.trim();
 
+        if ((inMessage = Validator
+                .fieldsAreNotBlank(username, password)) == null) {
             ProfileEntity user = new ProfileEntity(username, password);
 
-            if ((id = DAO.getId(user)) != null) {
-                logged = true;
-                resetData();
+            if ((inMessage = Validator
+                    .validationSignInId(id = DAO.getId(user))) == null) {
+                login();
                 registered = DAO.isRegistered(id);
                 return "/faces/profile.xhtml?faces-redirect=true&id=" + id;
             } else {
-                inMessage = "Incorrect username or password";
                 return null;
             }
         } else {
-            inMessage = "This fields cannot be blank";
             return null;
         }
     }
 
     public String signup() {
-        if (!"".equals(username.trim()) && !"".equals(password.trim())
-                && !"".equals(secondPassword.trim())) {
-            username = username.trim();
-            password = password.trim();
-            secondPassword = secondPassword.trim();
+        username = username.trim();
+        password = password.trim();
+        secondPassword = secondPassword.trim();
 
-            if (password.equals(secondPassword)) {
+        if ((upMessage = Validator.fieldsAreNotBlank(username,
+                password, secondPassword)) == null) {
+
+            if ((upMessage = Validator.validationUsername(username)) != null) {
+                return "/faces/index.xhtml?faces-redirect=true";
+            }
+
+            if ((upMessage = Validator.validationPassword(password)) != null) {
+                return "/faces/index.xhtml?faces-redirect=true";
+            }
+
+            if ((upMessage = Validator
+                    .passwordsAreEquals(password, secondPassword)) == null) {
                 ProfileEntity user = new ProfileEntity(username, password);
 
-                try {
-                    id = DAO.addUser(user);
-                    logged = true;
-                    resetData();
+                if ((upMessage = Validator
+                        .validationSignUpId(id = DAO.addUser(user))) == null) {
+                    login();
                     return "/faces/register.xhtml?faces-redirect=true";
-                } catch (PersistenceException e) {
-                    upMessage = "This username is already taken";
-                    return "/faces/index.xhtml?faces-redirect=true";
+                } else {
+                    return null;
                 }
             } else {
-                upMessage = "Passwords do not match";
-                return "/faces/index.xhtml?faces-redirect=true";
+                return null;
             }
         } else {
 
-            if ("".equals(username.trim()) || "".equals(password.trim())) {
-                upMessage = "This fields cannot be blank";
-                return "/faces/index.xhtml?faces-redirect=true";
+            if ((upMessage = Validator
+                    .fieldsAreNotBlank(username, password)) != null) {
+                return null;
             }
 
-            if ("".equals(secondPassword.trim())) {
-                upMessage = "Passwords do not match";
-                return "/faces/index.xhtml?faces-redirect=true";
+            if ((upMessage = Validator
+                    .fieldsAreNotBlank(secondPassword)) != null) {
+                return null;
             }
 
-            return "/faces/index.xhtml?faces-redirect=true";
+            upMessage = Validator.unhandledError();
+            return null;
         }
     }
 
     public String register() {
-        if (!"".equals(firstName.trim()) && !"".equals(lastName.trim())) {
-            firstName = firstName.trim();
-            lastName = lastName.trim();
-            if (photo != null && !"".equals(photo.trim())) {
-                photo = photo.trim();
-                try {
-                    checkPhoto();
+        firstName = firstName.trim();
+        lastName = lastName.trim();
+        photo = photo.trim();
 
-                } catch (IOException error) {
-                    photo = "";
-                    upMessage = error.getMessage();
-                    return "/faces/register.xhtml?faces-redirect=true";
-                }
+        if ((upMessage = Validator.fieldsAreNotBlank(firstName,
+                lastName)) == null) {
+
+            if ((upMessage = Validator
+                    .validationFullName(firstName, lastName)) != null) {
+                return null;
             }
+
+            if (Validator.fieldsAreNotBlank(photo) == null &&
+                    (upMessage = Validator.validationPhoto(photo)) != null) {
+                photo = "";
+                return null;
+            }
+
             DAO.register(id, firstName, lastName, photo);
             registered = true;
             return "/faces/profile.xhtml?faces-redirect=true&id=" + id;
         } else {
-            upMessage = "Full name cannot be blank";
-            return "/faces/register.xhtml?faces-redirect=true";
+            return null;
         }
 
+    }
+
+    private void login() {
+        logged = true;
+        username = null;
+        password = null;
+        secondPassword = null;
+        inMessage = null;
+        upMessage = null;
     }
 
     public String homePage() {
@@ -232,28 +239,5 @@ public class AuthorizationBean implements Serializable {
         session.removeAttribute("authorizationBean");
         session.invalidate();
         return "/faces/signin.xhtml?faces-redirect=true";
-    }
-
-    private void checkPhoto() throws IOException, IllegalArgumentException {
-        if (!photo.matches("^(https?|ftp)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;" +
-                "]*[-a-zA-Z0-9+&@#/%=~_|]")) {
-            throw new MalformedURLException("Invalid url");
-        }
-
-        URL url = new URL(photo);
-        BufferedImage image;
-        try {
-            image = ImageIO.read(url);
-        } catch (IOException error) {
-            throw new IOException("Invalid url");
-        }
-
-        if (image == null) {
-            throw new IOException("Invalid url");
-        }
-
-        if (image.getWidth() > 200 || image.getHeight() > 300) {
-            throw new IOException("It must be less than 200x300");
-        }
     }
 }
---
+++ src/main/java/com/kiselev/faces/validators/Validator.java
@@ -0,0 +1,124 @@
+package com.kiselev.faces.validators;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+public class Validator {
+
+    public static String fieldsAreNotBlank(String... fields) {
+        for (String field : fields) {
+            if (field.equals("")) {
+                return "This fields cannot be blank";
+            }
+        }
+        return null;
+    }
+
+    public static String validationUsername(String username) {
+
+        if (username.length() < 5) {
+            return "Username must contains at least 5 characters";
+        }
+
+        if (username.length() > 33) {
+            return "Username must not be longer than 33 characters";
+        }
+
+        if (countOfCharacters(username, '.') > 1 ||
+                !username.matches("^[a-zA-Z0-9][a-zA-Z0-9\\.]+[a-zA-Z0-9]$")) {
+            return "Username can contains letters, digits and one dot";
+        }
+
+        return null;
+    }
+
+    public static String validationSignInId(Long id) {
+        if (id == null) {
+            return "Incorrect username or password";
+        }
+        return null;
+    }
+
+    public static String validationSignUpId(Long id) {
+        if (id == null) {
+            return "This username is already taken";
+        }
+        return null;
+    }
+
+    public static String validationPassword(String password) {
+
+        if (password.length() < 6) {
+            return "Password must contains at least 6 characters";
+        }
+
+        return null;
+    }
+
+    public static String passwordsAreEquals(
+            String password, String secondPassword) {
+
+        if (!password.equals(secondPassword)) {
+            return "Passwords do not match";
+        }
+        return null;
+    }
+
+    public static String validationFullName(String firstName, String lastName) {
+
+        if (firstName.length() < 2 || lastName.length() < 2 ||
+                firstName.length() > 35 || lastName.length() > 35) {
+            return "Use your full and real name";
+        }
+
+        if (!firstName.matches("^[\\u00c0-\\u01ffA-ZА-Я]" +
+                "[\\u00c0-\\u01ffa-zA-Zа-яА-Я'\\-]+$") ||
+
+                !lastName.matches("^[\\u00c0-\\u01ffA-ZА-Я]" +
+                        "[\\u00c0-\\u01ffa-zA-Zа-яА-Я'\\-]+$")) {
+            return "Use your real name";
+        }
+
+        return null;
+    }
+
+    public static String validationPhoto(String photo) {
+
+        if (!photo.matches("^(https?|ftp)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;" +
+                "]*[-a-zA-Z0-9+&@#/%=~_|]")) {
+            return "Invalid url";
+        }
+        BufferedImage image;
+        try {
+            URL url = new URL(photo);
+            image = ImageIO.read(url);
+        } catch (IOException error) {
+            return "Invalid url";
+        }
+
+        if (image == null) {
+            return "Invalid url";
+        }
+
+        if (image.getWidth() > 200 || image.getHeight() > 300) {
+            return "It must be less than 200x300";
+        }
+
+        return null;
+    }
+
+    public static String unhandledError() {
+        return "Unhandled error";
+    }
+
+    private static int countOfCharacters(String username, char symbol) {
+        int count = 0;
+        for (int index = 0; index < username.length(); index++)
+            if (username.charAt(index) == symbol) {
+                count++;
+            }
+        return count;
+    }
+}
